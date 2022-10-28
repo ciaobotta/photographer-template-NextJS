1676cde9cfaef40276f8c1e1bb462d487bd36508
ciaobotta <69284419+ciaobotta@users.noreply.github.com>
2022-10-28 12:32:03 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //put moralis keys in the .env with wallet serverURL and application ID of moralis server set up
+  /*
+   click on View Details to get the information necessary to link our Next.js app 
+   to our Moralis server. Take note of the Server URL and Application ID.
+ 
+    <MoralisProvider></MoralisProvider>s
+  
+   */
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
